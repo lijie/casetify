0877b26a37750ead1dfcf49b2811495620578ab8
Li Jie <eltshanli@gmail.com>
2014-11-08 01:36:08 +0800

    improve

--- html/design.js
+++ html/design.js
@@ -54,29 +54,22 @@ function design_button_init() {
 }
 
 function design_panel_init() {
-//    var last_panel = $("#panel-device");
-//    var last_panel_fake = $("#panel-device-fake");
-//
-//    var panel_array = new Array($("#panel-photo"), $("#panel-device"), $("#panel-filter"));
-//    var panel_fake_array = new Array($("#panel-photo-fake"), $("#panel-device-fake"), $("#panel-filter-fake"));
-//    var btn_array = new Array($("#btn-select-photo"), $("#btn-select-device"), $("#btn-select-filter"));
-//
-//    for (i = 0; i < panel_array.length; i++) {
-//	panel_fake_array[i].show();
-//	panel_array[i].hide();
-//    }
-//    last_panel.show();
-//    last_panel_fake.hide();
-//    
-//    for (i = 0; i < panel_array.length; i++) {
-//	btn_array[i].on("click", function(event) {
-//	    console.log("i is", i);
-//	    last_panel.hide();
-//	    last_panel_fake.show();
-//	    panel_array[i].show();
-//	    panel_array_fake[i].hide();
-//	    last_panel = panel_array[i];
-//	    last_panel_fake = panel_fake_array[i];
-//	});
-//    }
+    var last_panel = $("#panel-device");
+
+    var panel_array = new Array($("#panel-photo"), $("#panel-device"), $("#panel-filter"));
+    var btn_array = new Array($("#btn-select-photo"), $("#btn-select-device"), $("#btn-select-filter"));
+
+    for (i = 0; i < panel_array.length; i++) {
+	panel_array[i].hide();
+    }
+    last_panel.show();
+    
+    for (i = 0; i < panel_array.length; i++) {
+	btn_array[i].on("click", null, i, function(event) {
+	    console.log("i is", event.data);
+	    last_panel.hide();
+	    panel_array[event.data].show();
+	    last_panel = panel_array[event.data];
+	});
+    }
 }
